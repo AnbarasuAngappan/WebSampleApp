5a6f4e099f528fc3dc861ae9383b43b9316ccbbf
Anbarasu Angappan <anburvhs@hotmail.com>
2018-06-11 12:46:48 +0530

    Add project files.

---
+++ WebSampleApp/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSampleApp.Models;
+
+namespace WebSampleApp.Controllers
+{
+    public class EmployeeController : Controller
+    {      
+        public ActionResult Details(string ID)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee = employeeContext.dbSetemployee.Single(Emp => Emp.EmployeeID == ID);
+            return View(employee);
+        }
+
+        public ActionResult HyperDetails()
+        {
+            EmployeeContext employeecontext = new EmployeeContext();
+            IList<Employee> employee = employeecontext.dbSetemployee.ToList();
+            return View(employee);
+        }
+    }
+}
---
+++ WebSampleApp/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebSampleApp.Models
+{
+    [Table("[Employeetable]")]
+    public class Employee
+    {
+        [Display(Name ="ID :")]
+        public string EmployeeID { get; set; }
+        [Display(Name ="Name :")]
+        public string EmployeeName { get; set; }
+        [Display(Name ="Age :")]
+        public string EmployeeAge { get; set; }
+        [Display(Name ="Gender :")]
+        public string EmployeeGender { get; set; }
+        [Display(Name ="City :")]
+        public string EmployeeCity { get; set; }
+    }
+}
